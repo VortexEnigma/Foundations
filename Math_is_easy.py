b86f8da6d91850191c1324482df1788f8db7778c
VortexEnigma <ggubien@gmail.com>
2025-01-09 13:43:46 +0100

    uploaded the files

---
+++ Math_is_easy.py
@@ -0,0 +1,26 @@
+# The Arithmetic operators, Math functions and their exercises
+from colorsys import yiq_to_rgb
+
+friends = 10
+
+#friends = friends + 1
+# OR
+#friends += 1
+
+#friends = friends - 2
+# OR
+#friends -= 2
+
+# etc. etc. Can do the same with  /= 2   **= 2
+# It is more efficient to use this method otherwise the code will be longer.
+#
+# The modulus operator gives you the remainder of a division %
+# remainder %= 3 (so the remainder will be 1 because 10 divided by 3)
+
+x = 3.14
+y = -4
+z = 5
+# result = round(x)
+result = abs(y)
+
+print(result)
---
+++ Variables.py
@@ -0,0 +1,55 @@
+#                      Variable = A container for a value (string, integer, float, boolean)
+#                               A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "George"
+last_name = "Gubien"
+name = first_name + " " + last_name
+food = "lasagna"
+
+# Integers
+age = 24
+quantity = 3
+num_of_students = 30
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.6
+
+# Boolean
+is_student = True
+has_studied = False
+
+#if is_student:
+#    print("You are a student")
+
+#else:
+#    print("You are not a student")
+
+#if has_studied:
+#    print("Good job!")
+
+#else:
+#    print("You suck")
+
+
+#                  Typecasting = The process of converting a value of one data type to another
+#                                         (string, integer, float, boolean)
+#     Explicit vs Implicit
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(is_student))
+# age = float(age)
+# print(type(age))
+# gpa = int(gpa)
+# print(gpa)
+
+
+
+x = 2
+y = 2.0
+
+x = x / y
